fbdb1829c31f5346fcf78760b9cfcad6a128489e
Domenico Monaco <monaco.d23@gmail.com>
2025-01-20 11:07:37 +0100

    welcome view reset

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,18 +18,22 @@
 
 
     <div class="container">
-
-
         <main>
-            
+            <section class="jumbotron text-center">
+                <div class="container">
+                    <h1>Laravel Address Book Example</h1>
+                    <p class="lead text-muted">Simple CRUD example about Addres Book</p>
+                    <p>
+                        <a class="btn btn-secondary" href='{{ route('contacts.list') }}'>Contatti</a>
+                    </p>
+                </div>
+            </section>
         </main>
-
-
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>

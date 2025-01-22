37b05e264d6427f9e124471d1171d8d4eeff63ab
Domenico Monaco <monaco.d23@gmail.com>
2025-01-22 17:43:10 +0100

    create layout

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,113 +1,78 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('title', 'New Address')
 
-    <title>Address Book Example</title>
+@section('content')
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <div class="row g-5">
 
-</head>
-
-<body>
-
-    <div class="container">
-
-        <div class="py-5 text-center">
-            <h2>Address book</h2>
-            <p class="lead"></p>
-        </div>
-
-        <div class="py-5 text-center">
+        <div class="text-center">
             <div class="d-flex justify-content-center">
-
-                <a class="btn btn-secondary" href='{{ route('contacts.list') }}'>Back to list</a>
+                <a class="btn btn-primary" href='{{ route('contacts.list') }}'>Back to list</a>
             </div>
         </div>
 
-        <div class="row g-5">
+        <div class="col-12">
+            <div>
+                @if ($errors->create->any())
+                    {!! implode('', $errors->create->all('<div class="alert alert-danger">:message</div>')) !!}
+                @endif
+            </div>
+            <h4 class="mb-3">Address item</h4>
+            <form action="{{ route('contacts.store') }}" method="post">
+                @csrf
+                <div class="row g-3">
+                    <div class="col-sm-6">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" name="name" id="name" placeholder="" value="{{ old('name') }}"
+                            required="" @class([
+                                'form-control' => true,
+                                'is-invalid' => $errors->create->has('name'),
+                            ])>
 
-            <div class="col-12">
-                <div>
-                    @if ($errors->create->any())
-                        {!! implode('', $errors->create->all('<div class="alert alert-danger">:message</div>')) !!}
-                    @endif
-                </div>
-                <h4 class="mb-3">Address item</h4>
-                <form action="{{ route('contacts.store') }}" method="post">
-                    @csrf
-                    <div class="row g-3">
-                        <div class="col-sm-6">
-                            <label for="name" class="form-label">Name</label>
-                            <input type="text"
-                                name="name" id="name" placeholder="" value="{{ old('name') }}" required="" 
-                                @class(['form-control'=>true,
-                               'is-invalid'=> $errors->create->has('name')])>
+                        @error('name', 'create')
+                            <div id="validationFeedbackName" class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
 
-                            @error('name', 'create')
-                                <div class="alert alert-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
+                    <div class="col-sm-6">
+                        <label for="second_name" class="form-label">Second name</label>
+                        <input type="text" class="form-control" name="second_name" id="second_name" placeholder=""
+                            value="{{ old('second_name') }}" required="">
+                    </div>
 
-                        <div class="col-sm-6">
-                            <label for="second_name" class="form-label">Second name</label>
-                            <input type="text" class="form-control" name="second_name" id="second_name"
-                                placeholder="" value="{{ old('second_name') }}" required="">
-                        </div>
+                    <div class="col-sm-12">
+                        <label for="address" class="form-label">Address</label>
+                        <input type="text" class="form-control" name="address" id="address" placeholder=""
+                            value="{{ old('address') }}" required="">
+                    </div>
 
-                        <div class="col-sm-12">
-                            <label for="address" class="form-label">Address</label>
-                            <input type="text" class="form-control" name="address" id="address" placeholder=""
-                                value="{{ old('address') }}" required="">
-                        </div>
+                    <div class="col-sm-5">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="text" class="form-control" name="email" id="email" placeholder=""
+                            value="{{ old('email') }}" required="">
+                    </div>
 
-                        <div class="col-sm-5">
-                            <label for="email" class="form-label">Email</label>
-                            <input type="text" class="form-control" name="email" id="email" placeholder=""
-                                value="{{ old('email') }}" required="">
-                        </div>
-
-                        <div class="col-sm-5">
-                            <label for="phonenumber" class="form-label">Phone</label>
-                            <input type="text" class="form-control" name="phonenumber" id="phonenumber"
-                                placeholder="" value="{{ old('phonenumber') }}" required="">
-
-                        </div>
-
-                        <div class="col-sm-2">
-                            <label for="years" class="form-label">Years</label>
-                            <input type="number" class="form-control" name="years" id="years" placeholder=""
-                                value="{{ old('years') }}" required="">
-
-                        </div>
+                    <div class="col-sm-5">
+                        <label for="phonenumber" class="form-label">Phone</label>
+                        <input type="text" class="form-control" name="phonenumber" id="phonenumber" placeholder=""
+                            value="{{ old('phonenumber') }}" required="">
 
                     </div>
 
-                    <hr class="my-4">
+                    <div class="col-sm-2">
+                        <label for="years" class="form-label">Years</label>
+                        <input type="number" class="form-control" name="years" id="years" placeholder=""
+                            value="{{ old('years') }}" required="">
 
-                    <button class="w-100 btn btn-primary btn-lg" type="submit">Save</button>
-                </form>
-            </div>
+                    </div>
+
+                </div>
+
+                <hr class="my-4">
+
+                <button class="w-100 btn btn-primary btn-lg" type="submit">Save</button>
+            </form>
         </div>
-
-
-
-
     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-
-    <script>
-        $(document).ready(function() {
-            console.log('Jquery');
-        });
-    </script>
-</body>
-
-</html>
+@endsection
--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,88 +1,57 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('title', 'List')
 
-    <title>Laravel</title>
+@section('content')
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-</head>
 
-<body>
-
-    <div class="container">
-
-        <div class="py-5 text-center">
-            <h2>Rubrica</h2>
-            <p class="lead"></p>
-        </div>
-
-        <div class="py-5 text-center">
+    <div class="row g-5">
+        <div class="text-center">
             <div class="d-flex justify-content-center">
-                <a class="btn btn-primary"  href='{{ route('contacts.create') }}'>Nuovo</a>
+                <a class="btn btn-primary" href='{{ route('contacts.create') }}'>New Address</a>
             </div>
         </div>
 
-        <div class="row g-5">
+        <div class="col-12">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Congome</th>
+                        <th scope="col">Indirizzo</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Telefono</th>
+                        <th scope="col">Età</th>
+                        <th scope="col">OPT</th>
+                    </tr>
+                </thead>
+                <tbody>
 
-            <div class="col-12">
-                <table class="table">
-                    <thead>
+                    @foreach ($contactslist as $item)
                         <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Nome</th>
-                            <th scope="col">Congome</th>
-                            <th scope="col">Indirizzo</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Telefono</th>
-                            <th scope="col">Età</th>
-                            <th scope="col">OPT</th>
+                            <th scope="row">1</th>
+                            <td>{{ $item->name }}</td>
+                            <td>{{ $item->second_name }}</td>
+                            <td>{{ $item->address }}</td>
+                            <td>{{ $item->email }}</td>
+                            <td>{{ $item->phonenumber }}</td>
+                            <td>{{ $item->years }}</td>
+                            <td>
+                                <a class="btn btn-sm btn-warning" href="/">Modifica</a>
+
+                                <form action="{{ route('contacts.destroy', $item->id) }}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                                </form>
+                            </td>
                         </tr>
-                    </thead>
-                    <tbody>
+                    @endforeach
 
-                        @foreach ($contactslist as $item)
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>{{ $item->name }}</td>
-                                <td>{{ $item->second_name }}</td>
-                                <td>{{ $item->address }}</td>
-                                <td>{{ $item->email }}</td>
-                                <td>{{ $item->phonenumber }}</td>
-                                <td>{{ $item->years }}</td>
-                                <td>
-                                    <a class="btn btn-sm btn-warning" href="/">Modifica</a>
-
-                                    <form action="{{ route('contacts.destroy', $item->id) }}" method="post">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-danger btn-sm">Delete</button>
-                                    </form>
-                                </td>
-                            </tr>
-                        @endforeach
-
-                    </tbody>
-                </table>
-            </div>
+                </tbody>
+            </table>
         </div>
-
     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-
-    <script>
-        $(document).ready(function() {
-            console.log('Jquery');
-        });
-    </script>
-</body>
-
-</html>
+@endsection
